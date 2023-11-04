0a73b50970ba81c7828e02ef90b5a6d072210d74
rachidassouani <rachidassouani@gmail.com>
2023-11-04 17:39:35 +0100

    init the project

---
+++ src/test/java/io/rachidassouani/booktracker/BooktrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.rachidassouani.booktracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooktrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
