3484312eb246aff20c1d9d7a2326a274d935b7df
rachidassouani <rachidassouani@gmail.com>
2023-11-05 16:25:58 +0100

    added docker compose file and configs to connect to cassandra

--- src/main/java/io/rachidassouani/booktracker/BooktrackerApplication.java
+++ src/main/java/io/rachidassouani/booktracker/BooktrackerApplication.java
@@ -1,13 +1,26 @@
 package io.rachidassouani.booktracker;
 
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BooktrackerApplication {
 
+	@Value("${datadump.location.author}")
+	private String authorDumpLocation;
+
+	@Value("${datadump.location.works}")
+	private String worksDumpLocation;
+
 	public static void main(String[] args) {
 		SpringApplication.run(BooktrackerApplication.class, args);
 	}
 
+	@PostConstruct
+	public void start() {
+		System.out.println(authorDumpLocation);
+		System.out.println(worksDumpLocation);
+	}
 }
