8a45ab3b04845ff83ee8e9f7d91710782b2d659b
rachidassouani <rachidassouani@gmail.com>
2023-11-05 17:49:54 +0100

    created author entity + repository + saving authors into cassandra

--- src/main/java/io/rachidassouani/booktracker/BooktrackerApplication.java
+++ src/main/java/io/rachidassouani/booktracker/BooktrackerApplication.java
@@ -1,9 +1,19 @@
 package io.rachidassouani.booktracker;
 
+import io.rachidassouani.booktracker.author.Author;
+import io.rachidassouani.booktracker.author.AuthorRepository;
 import jakarta.annotation.PostConstruct;
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
 
 @SpringBootApplication
 public class BooktrackerApplication {
@@ -14,13 +24,39 @@ public class BooktrackerApplication {
 	@Value("${datadump.location.works}")
 	private String worksDumpLocation;
 
+	@Autowired
+	private AuthorRepository authorRepository;
+
 	public static void main(String[] args) {
 		SpringApplication.run(BooktrackerApplication.class, args);
 	}
 
+	private void initAuthors() {
+		Path path = Paths.get(authorDumpLocation);
+		try (Stream<String> lines = Files.lines(path)) {
+			lines.forEach(line -> {
+				// read line
+				String jsonString = line.substring(line.indexOf("{"));
+				// parse it into json
+				JSONObject jsonObject = new JSONObject(jsonString);
+
+				// new author object
+				Author author = new Author();
+				author.setId(jsonObject.optString("key").replace("/authors/", ""));
+				author.setName(jsonObject.optString("name"));
+
+				// saving author
+				authorRepository.save(author);
+
+				System.out.println(author.getName() + " saved successfully");
+			});
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
 	@PostConstruct
 	public void start() {
-		System.out.println(authorDumpLocation);
-		System.out.println(worksDumpLocation);
+		initAuthors();
 	}
 }
---
+++ src/main/java/io/rachidassouani/booktracker/author/Author.java
@@ -0,0 +1,37 @@
+package io.rachidassouani.booktracker.author;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.cassandra.core.cql.PrimaryKeyType;
+import org.springframework.data.cassandra.core.mapping.CassandraType;
+import org.springframework.data.cassandra.core.mapping.Column;
+import org.springframework.data.cassandra.core.mapping.PrimaryKeyColumn;
+import org.springframework.data.cassandra.core.mapping.Table;
+
+@Table("author_by_id")
+public class Author {
+
+    @Id
+    @PrimaryKeyColumn(value = "author_id", ordinal = 0, type = PrimaryKeyType.PARTITIONED)
+    private String id;
+
+    @Column("author_name")
+    @CassandraType(type = CassandraType.Name.TEXT)
+    private String name;
+
+    // getters and setters
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
